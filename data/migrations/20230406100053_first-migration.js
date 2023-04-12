a31b9be9d43ea21da1e8f8acdc9a338a1f78cf1e
Joshua Kerr <kerrjoshua@gmail.com>
2023-04-12 16:31:52 +0900

    setting up first migration for guitar database

--- data/migrations/20230406100053_first-migration.js
+++ data/migrations/20230406100053_first-migration.js
@@ -1,5 +1,24 @@
 
 exports.up = function(knex) {
+    return knex.schema
+    .createTable('brands', brands => {
+        brands.increments('brand_id')
+        brands.string('brand_name').notNullable().unique()
+    })
+    .createTable('models', models => {
+        models.increments('model_id')
+        models.string('model_name')
+    })
+    .createTable('guitars', guitars => {
+        guitars.increments('guitar_id')
+        guitars.string('guitar_name').notNullable().unique()
+        guitars.integer('make_id')
+            .notNullable()
+            .unsigned()
+            .references('make_id')
+            .inTable('makes')
+        guitars.string('guitar_model').notNullable()
+    })
   
 };
 
