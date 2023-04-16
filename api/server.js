f9b6050ba70aa70bd38538746855f58f06a681d4
Joshua Kerr <kerrjoshua@gmail.com>
2023-04-16 16:44:25 +0900

    wip

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const guitarsRouter = require('./guitars/guitars-router')
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/guitars", guitarsRouter);
+
+server.use((err, req, res, next) => { 
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
